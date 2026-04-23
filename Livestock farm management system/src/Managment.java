594d66fc6fb7c10e0e8d469489617ecb3821751a
oni <140212282+yacine1478@users.noreply.github.com>
2026-04-23 12:59:43 +0100

    Add files via upload

--- Livestock farm management system/src/Managment.java
+++ Livestock farm management system/src/Managment.java
@@ -20,9 +20,11 @@ public class Managment {
         System.out.println("*****************");
         System.out.println("5. Mannage the History of buy and sell");
         System.out.println("*****************");
+        System.out.println("Tracking Of Animal");
+        System.out.println("*****************");
         System.out.println("exit");
         System.out.println("*****************");
-        System.out.printf("- enter your choice (1-6) - ");
+        System.out.printf("- enter your choice (1-7) - ");
         choice = s1.nextInt();
         switch (choice) {
             case 1 -> workerManager.manageWorkers();
@@ -30,7 +32,8 @@ public class Managment {
             case 3 -> System.out.println("History of Medical Treatments");
             case 4 -> System.out.println("Type of Animals");
             case 5 -> System.out.println("Manage the History of buy and sell");
-            case 6 -> isRunning = false;
+            case 6 -> System.out.println("Tracking Of Animal");
+            case 7 -> isRunning = false;
             default -> System.out.println("invalid choice");
 
 
